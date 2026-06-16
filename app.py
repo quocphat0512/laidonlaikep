1db65748e23492dd6d4df53f0242d1e0e54fcb5f
quocphat0512 <quocphat0512@gmail.com>
2026-06-16 08:47:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Tiêu đề ứng dụng
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm")
+st.title("💰 Ứng dụng tính tiền gửi tiết kiệm_NGUYỄN QUỐC PHÁT")
 
 # Nhập dữ liệu
 C = st.number_input(
